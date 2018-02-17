1dbba6f357749efbcffbbfe3cced261de04330fa
Kinjo <amsp8b@mst.edu>
2018-02-16 21:42:47 -0600

    Player constantly moves forward

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,24 +5,30 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour 
 {
 	private Rigidbody2D body;
-	private Vector2 jumpVelocity = new Vector2(0, 6);
-	private Vector3 groundedVector = new Vector3(0, -1.0f, 0);
+	private Vector2 jumpVelocity = new Vector2(0, 300);
+	private Vector3 groundedVector = new Vector3(0, -0.7f, 0);
+	private Vector2 velocity = new Vector2(2,0);
 
 	// Use this for initialization
 	void Start () 
 	{
 		body = this.gameObject.GetComponent<Rigidbody2D>();
+		body.velocity = velocity;
 	}
-	
+
 	// Update is called once per frame
 	void Update () 
 	{
 		Debug.DrawLine(transform.position, transform.position + groundedVector, Color.yellow);
 
+		// Jump
 		if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
 		{
-			body.velocity = jumpVelocity;
+			body.AddForce(jumpVelocity);
 		}
+
+		// Move forward at a constant speed
+		body.velocity = new Vector2(velocity.x,  body.velocity.y);
 	}
 
 	bool isGrounded()
