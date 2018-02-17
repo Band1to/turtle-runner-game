ba24f101cd6e315e90cb5d74a329ac49bdc8a3c8
Kinjo <amsp8b@mst.edu>
2018-02-16 20:28:52 -0600

    Can only jump when grounded

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
 	private Rigidbody2D body;
 	private Vector2 jumpVelocity = new Vector2(0, 6);
+	private Vector3 groundedVector = new Vector3(0, -1.0f, 0);
 
 	// Use this for initialization
 	void Start () 
@@ -16,9 +17,17 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.GetKeyDown(KeyCode.Space))
+		Debug.DrawLine(transform.position, transform.position + groundedVector, Color.yellow);
+
+		if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
 		{
 			body.velocity = jumpVelocity;
 		}
+	}
+
+	bool isGrounded()
+	{
+		int blockedMask = (1 << LayerMask.NameToLayer("Impassable"));
+		return Physics2D.Linecast(transform.position, transform.position + groundedVector, blockedMask);
 	}
 }
