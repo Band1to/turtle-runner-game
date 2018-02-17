3724757380afd2be4e8d40199297a55aa5f9f610
Kinjo <amsp8b@mst.edu>
2018-02-16 20:20:05 -0600

    Player can jump

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+	private Rigidbody2D body;
+	private Vector2 jumpVelocity = new Vector2(0, 6);
+
+	// Use this for initialization
+	void Start () 
+	{
+		body = this.gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			body.velocity = jumpVelocity;
+		}
+	}
+}
